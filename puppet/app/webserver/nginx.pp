62b75abc9039d83e4cf7c7837e3f27aa81112de6
benatespina <benatespina@gmail.com>
2014-06-27 13:46:28 +0200

    Fixed an issue with fastcgi and added some improvements into Nginx

--- puppet/app/webserver/nginx.pp
+++ puppet/app/webserver/nginx.pp
@@ -1,7 +1,6 @@
 class app::webserver::nginxserver {
-require app::ssl
-class { 'nginx': }
-
+    require app::ssl
+    class { 'nginx': }
 
     case $::osfamily {
         Redhat: {
@@ -16,46 +15,53 @@ class { 'nginx': }
         ensure => purged,
     }
 
-nginx::resource::vhost { "$vhost.$domain":
-  ensure => 'present',
-  www_root => "$vhostpath/$vhost.$domain/web",
-  index_files => [ 'app_dev.php'],
-  try_files   => [ '$uri', '@rewriteindex'],
-  ssl         => true,
-  ssl_cert    => "/etc/ssl/private/$vhost$domain.crt",
-  ssl_key     => "/etc/ssl/private/$vhost$domain.key",
-  access_log  => "$vhostpath/$vhost.$domain/app/logs/access.log",
-  error_log   => "$vhostpath/$vhost.$domain/app/logs/error.log",
-}
+    nginx::resource::vhost { "$vhost.$domain":
+        ensure      => 'present',
+        www_root    => "$vhostpath/$vhost.$domain/web",
+        index_files => [ 'app_dev.php'],
+        try_files   => [ '$uri', '@rewriteindex'],
+        ssl         => true,
+        ssl_cert    => "/etc/ssl/private/$vhost$domain.crt",
+        ssl_key     => "/etc/ssl/private/$vhost$domain.key",
+        access_log  => "/var/log/nginx/access.log",
+        error_log   => "/var/log/nginx/error.log",
+    }
 
-nginx::resource::location { 'location-rewriteindex':
-    ensure  => present,
-    ssl     => true,
-    vhost   => "$vhost.$domain",
-    www_root    => "$vhostpath/$vhost.$domain/web",
-    location    => '@rewriteindex',
-    rewrite_rules => ['^(.*)$ /app.php/$1 last'],
+    nginx::resource::location { 'location-rewriteindex':
+        ensure        => present,
+        ssl           => true,
+        vhost         => "$vhost.$domain",
+        www_root      => "$vhostpath/$vhost.$domain/web",
+        location      => '@rewriteindex',
+        rewrite_rules => ['^(.*)$ /app.php/$1 last'],
 
-}
+    }
 
 
-nginx::resource::location { 'location-phpfpm':
-      ensure          => present,
-      ssl             => true,
-      vhost           => "$vhost.$domain",
-      www_root        => "$vhostpath/$vhost.$domain/web",
-      location        => '~ ^/(app|app_dev)\.php(/|$)',
-      proxy           => undef,
-      fastcgi         => "unix:/run/shm/$vhost$domain.phpfpm.socket",
-      location_cfg_append => {
-        fastcgi_connect_timeout => '3m',
-        fastcgi_read_timeout    => '3m',
-        fastcgi_send_timeout    => '3m',
-        fastcgi_split_path_info => '^(.+\.php)(/.*)$',
-        include                 => 'fastcgi_params',
-        fastcgi_param           =>  'SCRIPT_FILENAME $document_root$fastcgi_script_name',
-      }
-}
-    
+    nginx::resource::location { 'location-phpfpm':
+        ensure          => present,
+        ssl             => true,
+        vhost           => "$vhost.$domain",
+        www_root        => "$vhostpath/$vhost.$domain/web",
+        location        => '~ ^/(app|app_dev)\.php(/|$)',
+        proxy           => undef,
+        fastcgi         => "127.0.0.1:9000",
+        location_cfg_append => {
+            fastcgi_connect_timeout => '3m',
+            fastcgi_read_timeout    => '3m',
+            fastcgi_send_timeout    => '3m',
+            fastcgi_split_path_info => '^(.+\.php)(/.*)$',
+            include                 => 'fastcgi_params',
+            fastcgi_param           => 'SCRIPT_FILENAME $document_root$fastcgi_script_name',
+        }
+    }
 
+    file {'/etc/nginx/conf.d/php5-fpm.nginx.conf':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        content => template("/vagrant/files/etc/nginx/conf.d/php5-fpm.nginx.conf"),
+        require => [File["/etc/nginx/conf.d/"]],
+        notify  => Service["php5-fpm", "nginx"],
+    }
 }
