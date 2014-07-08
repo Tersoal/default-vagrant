ce502d9ee60dd5c20341d25caa80f6a933f1ab39
benatespina <benatespina@gmail.com>
2014-07-08 17:30:04 +0200

    Added xdebug module and fixed some issues with PHP5.5 and FPM

--- puppet/app/php.pp
+++ puppet/app/php.pp
@@ -2,17 +2,29 @@ class app::php {
     package {
         [
             "php5-cli",
+            "php5-mysql",
             # "php5-apcu",
             "php5-intl",
             "php5-curl",
             "php5-mcrypt",
-            "php5-xdebug",
             "php5-memcached",
             "php-pear",
         ]:
         ensure => present,
         notify => Service[$webserverService],
     }
+
+    class { 'xdebug':
+        default_enable   => '1',
+        remote_enable    => '1',
+        remote_handler   => 'dbgp',
+        remote_host      => 'localhost',
+        remote_port      => '9000',
+        remote_autostart => '1',
+        require => [Package["php5-cli"]],
+        notify => Service[$webserverService],
+    }
+
 
     exec {'change-permissions':
         require => File['/dev/shm/symfony', '/dev/shm/symfony/cache', '/dev/shm/symfony/logs'],
@@ -29,11 +41,7 @@ class app::php {
         command => 'sudo chmod -R 777 /dev/shm/symfony',
     }
 
-    if 'nginx' == $webserver {
-        include app::php::fpm
-    }
-
-    include app::php::phpqatools
+    include app::php::fpm
 
     include composer
 }
--- puppet/app/php/fpm.pp
+++ puppet/app/php/fpm.pp
@@ -4,22 +4,6 @@ class app::php::fpm {
         notify => Service[$webserver],
     }
 
-    file {"/etc/php5/fpm/pool.d":
-        ensure => directory,
-        owner => root,
-        group => root,
-        require => [Package["php5-fpm"]],
-    }
-
-    file {"/etc/php5/fpm/pool.d/$vhost$domain.conf":
-        ensure => present,
-        owner => root,
-        group => root,
-        content => template("/vagrant/files/etc/php5/fpm/pool.d/template.erb"),
-        require => [File["/etc/php5/fpm/pool.d"]],
-        notify => Service["php5-fpm", "nginx"],
-    }
-
     file {'/etc/php5/fpm/conf.d/my-php.ini':
         ensure  => 'present',
         owner   => root,
