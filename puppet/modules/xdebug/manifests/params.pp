67f4821a767a80e825d1195caab16a800815dbe1
benatespina <benatespina@gmail.com>
2014-07-25 13:47:30 +0200

    Added proper Xdebug module

---
+++ puppet/modules/xdebug/manifests/params.pp
@@ -0,0 +1,26 @@
+class xdebug::params {
+  case $operatingsystem {
+    'RedHat', 'CentOS', 'Fedora': {
+      $ini_file_path = '/etc/php.d/xdebug.ini'
+      $package = 'php-pecl-xdebug'
+      $php = 'php-cli'
+      $zend_extension_module = '/usr/lib64/php/modules/xdebug.so'
+    }
+    'Debian', 'Ubuntu': {
+      $ini_file_path = '/etc/php5/conf.d/xdebug_config.ini'
+      $package = 'php5-xdebug'
+      $php = 'php5-cli'
+      $zend_extension_module = '/usr/lib64/php/modules/xdebug.so'
+    }
+  }
+
+  $default_enable      = '1'
+  $remote_enable       = '1'
+  $remote_handler      = 'dbgp'
+  $remote_host         = 'localhost'
+  $remote_port         = '9000'
+  $remote_autostart    = '1'
+  $remote_connect_back = '0'
+  $remote_log          = false
+  $idekey              = ''
+}
