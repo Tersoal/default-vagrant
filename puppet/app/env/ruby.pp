6b17d621321ad01e7e79ca83d6d3b3f117431a7f
benatespina <benatespina@gmail.com>
2015-02-25 09:13:46 +0100

    Upgraded ruby version to 1.9.3 and added scss-lint

--- puppet/app/env/ruby.pp
+++ puppet/app/env/ruby.pp
@@ -1,11 +1,27 @@
 class app::env::ruby {
+    exec {'apt-get-update':
+        command => '/usr/bin/apt-get update',
+    }
+
+    exec { 'install ruby':
+        command => '/usr/bin/apt-get install ruby1.9.3 -y',
+        require => Exec['apt-get-update'],
+    }
+
     package { 'sass':
         ensure   => $sass_version,
         provider => 'gem',
+        require => Exec['install ruby']
+    }
+
+    package { 'scss-lint':
+        provider => 'gem',
+        require => Exec['install ruby']
     }
 
     package { 'compass':
         ensure => $compass_version,
         provider => 'gem',
+        require => Exec['install ruby']
     }
 }
